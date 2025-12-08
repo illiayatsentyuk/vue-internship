fe67f9005ddd68f7658f0e3b66777c5669e02840
illiayatsentyuk@gmail.com <illiayatsentyuk@gmail.com>
2025-12-08 19:39:10 +0200

    Refactor AuthForm component: improve code formatting, enhance sign-up functionality with additional input fields, and update styles for better user experience.

--- 2/src/components/AuthForm.vue
+++ 2/src/components/AuthForm.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import {ref} from "vue"
+import { ref } from "vue"
 import { AuthModes } from "@/enums";
 
 const mode = ref<AuthModes>(AuthModes.SIGNIN)
 
-function changeAuthMode(){
+function changeAuthMode() {
     mode.value = mode.value === AuthModes.SIGNIN ? AuthModes.SIGNUP : AuthModes.SIGNIN
 }
 
@@ -20,7 +20,7 @@ function changeAuthMode(){
                 <p>Manage your tasks and projects efficiently</p>
             </div>
         </div>
-        <div v-if="mode===AuthModes.SIGNIN" class="auth-section__form-section">
+        <div v-if="mode === AuthModes.SIGNIN" class="auth-section__form-section">
             <h1 class="auth-section__form-title">Sign in to SynTask</h1>
             <p class="auth-section__form-subtitle">Start managing your tasks efficiently</p>
             <form action="#" class="auth-section__form">
@@ -42,19 +42,106 @@ function changeAuthMode(){
                     <img src="@/assets/google.svg" alt="">
                     <p>Google</p>
                 </div>
-                <div class="auth-section__form-section__types-of-login__item">                   
+                <div class="auth-section__form-section__types-of-login__item">
                     <img src="@/assets/github.svg" alt="">
                     <p>GitHub</p>
                 </div>
             </div>
             <p class="dont-have-account">Don't have an account? <span @click="changeAuthMode">Register now</span></p>
         </div>
-        <div v-if="mode===AuthModes.SIGNUP" class="auth-section__form-section">SignUp</div>
+        <div v-if="mode === AuthModes.SIGNUP" class="auth-signup-section__form-section auth-section__form-section">
+            <h1 class="auth-section__form-title">Create your account</h1>
+            <p class="auth-section__form-subtitle">Start managing your tasks efficiently</p>
+            <form action="#" class="auth-section__form">
+                <div class="auth-section__form-input__section">
+                    <div class="auth-section__form-input__section-name_section">
+                        <div>
+                            <label for="firstName">First Name</label>
+                            <input type="text" name="firstName">
+                        </div>
+                        <div>
+                            <label for="lastName">Last Name</label>
+                            <input type="text" name="lastName">
+                        </div>
+                    </div>
+                    <label for="email" class="email-label">Email address</label>
+                    <input name="email" type="text" class="email-input">
+                    <label for="password" class="password-label">Password</label>
+                    <input name="password" class="password-input" type="password">
+                    <label for="confirmPassword" class="password-label">Confirm Password</label>
+                    <input name="confirmPassword" class="password-input" type="password">
+                </div>
+                <button>Sign Up</button>
+            </form>
+            <p class="continue-with">Or continue with</p>
+            <div class="auth-section__form-section__types-of-login">
+                <div class="auth-section__form-section__types-of-login__item">
+                    <img src="@/assets/google.svg" alt="">
+                    <p>Google</p>
+                </div>
+                <div class="auth-section__form-section__types-of-login__item">
+                    <img src="@/assets/github.svg" alt="">
+                    <p>GitHub</p>
+                </div>
+            </div>
+            <p class="dont-have-account">Already have an account? <span @click="changeAuthMode">SignIn</span></p>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.dont-have-account{
+.auth-signup-section__form-section>h1 {
+    font-family: Inter;
+    font-weight: 700;
+    font-style: Bold;
+    font-size: 36px;
+    leading-trim: NONE;
+    line-height: 36px;
+    letter-spacing: 0%;
+    text-align: center;
+    color:#111827;
+    margin-bottom:10px;
+}
+
+.auth-signup-section__form-section>p {
+    font-family: Inter;
+    font-weight: 400;
+    font-style: Regular;
+    font-size: 20px;
+    leading-trim: NONE;
+    line-height: 20px;
+    letter-spacing: 0%;
+    text-align: center;
+    margin-bottom:24px;
+    color:#4B5563;
+}
+.auth-section__form-input__section-name_section{
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom:16px;
+}
+.auth-section__form-input__section-name_section > div{
+    display: flex;
+    flex-direction: column;
+}
+.auth-section__form-input__section-name_section > div > label{
+    font-family: Inter;
+font-weight: 500;
+font-style: Medium;
+font-size: 14px;
+leading-trim: NONE;
+line-height: 100%;
+letter-spacing: 0%;
+color:#374151;
+}
+.auth-section__form-input__section-name_section > div > input {
+    padding: 13px 16px;
+    border-radius: 8px;
+    border:1px solid #D1D5DB;
+}
+.dont-have-account {
     font-family: Inter;
     font-weight: 400;
     font-style: Regular;
@@ -63,15 +150,16 @@ function changeAuthMode(){
     line-height: 14px;
     letter-spacing: 0%;
     text-align: center;
-    color:#4b5563;
-}
-.dont-have-account > span{
-    font-weight: 500;
-    color:#4F46E5;
-    cursor:pointer
+    color: #4b5563;
 }
 
-.continue-with{
+.dont-have-account>span {
+    font-weight: 500;
+    color: #4F46E5;
+    cursor: pointer
+}
+
+.continue-with {
     font-family: Inter;
     font-weight: 400;
     font-style: Regular;
@@ -79,22 +167,25 @@ function changeAuthMode(){
     leading-trim: NONE;
     line-height: 100%;
     letter-spacing: 0%;
-    color:#6B7280;
+    color: #6B7280;
 }
-.auth-section__form-section__types-of-login{
+
+.auth-section__form-section__types-of-login {
     display: flex;
     width: 100%;
     justify-content: space-between;
     align-items: center;
-    margin-bottom:25px;
+    margin-bottom: 25px;
 }
-.auth-section__form-section__types-of-login__item{
+
+.auth-section__form-section__types-of-login__item {
     display: flex;
     align-items: center;
     justify-content: center;
-    gap:7px;
-    padding:14px 70px;
-    border:1px solid #D1D5DB;
+    gap: 7px;
+    width:216px;
+    height: 46px;
+    border: 1px solid #D1D5DB;
     box-shadow: 0px 1px 2px 0px #0000000D;
     border-radius: 8px;
     font-family: Inter;
@@ -108,42 +199,48 @@ function changeAuthMode(){
     color: #374151;
     cursor: pointer;
 }
-.auth-section__form-section__types-of-login__item:hover{
-    filter:brightness(0.8);
+
+.auth-section__form-section__types-of-login__item:hover {
+    filter: brightness(0.8);
 }
-.auth-section{
+
+.auth-section {
     height: 100vh;
     display: flex;
 }
-.auth-section__information{
+
+.auth-section__information {
     display: flex;
     align-items: center;
     justify-content: center;
     position: relative;
-    width:50%;
-    height: 100%;
-}
-.auth-form{
-    width:50%;
+    width: 50%;
     height: 100%;
 }
 
-.auth-section__signin-image{
+.auth-form {
+    width: 50%;
+    height: 100%;
+}
+
+.auth-section__signin-image {
     position: absolute;
-    top:0;
-    left:0;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
-    filter:brightness(0.8);
-    z-index:-999;
+    filter: brightness(0.8);
+    z-index: -999;
 }
-.auth-section__text{
+
+.auth-section__text {
     display: flex;
     flex-direction: column;
-    gap:16px;
-    color:#ffffff;
+    gap: 16px;
+    color: #ffffff;
 }
-.auth-section__text > h1{
+
+.auth-section__text>h1 {
     font-family: Inter;
     font-weight: 700;
     font-style: Bold;
@@ -153,7 +250,8 @@ function changeAuthMode(){
     letter-spacing: 0%;
     text-align: center;
 }
-.auth-section__text > p{
+
+.auth-section__text>p {
     font-family: Inter;
     font-weight: 400;
     font-style: Regular;
@@ -163,16 +261,25 @@ function changeAuthMode(){
     letter-spacing: 0%;
     text-align: center;
 }
-.auth-section__form-section{
+.auth-signup-section__form-section{
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding:132px 152px;
-    width:50%;
+    padding: 71px 132px;
+    width: 50%;
 }
-.auth-section__form-section > h1{
-    margin-bottom:10px;
+.auth-section__form-section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 132px 152px;
+    width: 50%;
+}
+
+.auth-section__form-section>h1 {
+    margin-bottom: 10px;
     color: #111827;
     font-family: Inter;
     font-weight: 700;
@@ -184,7 +291,8 @@ function changeAuthMode(){
     text-align: center;
 
 }
-.auth-section__form-section > p{
+
+.auth-section__form-section>p {
     margin-bottom: 34px;
     font-family: Inter;
     font-weight: 400;
@@ -196,34 +304,38 @@ function changeAuthMode(){
     text-align: center;
 
 }
-.auth-section__form{
-    display:flex;
+
+.auth-section__form {
+    display: flex;
     flex-direction: column;
-    width:100%;
+    width: 100%;
 }
-.auth-section__form-input__section{
-    width:100%;
+
+.auth-section__form-input__section {
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
 }
-.auth-section__form-chexbox{
+
+.auth-section__form-chexbox {
     display: flex;
-    gap:8px;
-    margin-bottom:24px;
+    gap: 8px;
+    margin-bottom: 24px;
 }
 
-.auth-section__form-input__section > input{
-    padding:13px 16px;
+.auth-section__form-input__section>input {
+    padding: 13px 16px;
     border-radius: 8px;
-    border:1px solid #D1D5DB;
-    margin-bottom:16px;
-}
-.auth-section__form-input__section > input:last-child{
-    margin-bottom:24px;
+    border: 1px solid #D1D5DB;
+    margin-bottom: 16px;
 }
 
-.auth-section__form-input__section > label{
+.auth-section__form-input__section>input:last-child {
+    margin-bottom: 24px;
+}
+
+.auth-section__form-input__section>label {
     font-family: Inter;
     font-weight: 500;
     font-style: Medium;
@@ -231,10 +343,11 @@ function changeAuthMode(){
     leading-trim: NONE;
     line-height: 100%;
     letter-spacing: 0%;
-    color:#374151;
-    margin-bottom:6px;
+    color: #374151;
+    margin-bottom: 6px;
 }
-.auth-section__form-chexbox > p{
+
+.auth-section__form-chexbox>p {
     font-family: Inter;
     font-weight: 400;
     font-style: Regular;
@@ -244,9 +357,9 @@ function changeAuthMode(){
     letter-spacing: 0%;
 }
 
-.auth-section__form > button {
+.auth-section__form>button {
     background: #4F46E5;
-    color:#fff;
+    color: #fff;
     font-family: Inter;
     font-weight: 500;
     font-style: Medium;
@@ -258,11 +371,12 @@ function changeAuthMode(){
     border: none;
     box-shadow: 0px 1px 2px 0px #0000000D;
     border-radius: 8px;
-    width:100%;
-    padding:14px;
-    margin-bottom:24px;
+    width: 100%;
+    padding: 14px;
+    margin-bottom: 24px;
 }
-.auth-section__form > button:hover{
-    filter:brightness(0.8)
+
+.auth-section__form>button:hover {
+    filter: brightness(0.8)
 }
 </style>
